1831fbec0a328a0923de10f2ec6b131b17d216c7
Isaac Byrne <me@ipbyrne.com>
2023-06-07 13:28:58 -0500

    add encrypt flexability

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {
   createEncryptedObject,
   createDecryptedObject,
 } from "./utils";
-import { decrypt, generate } from "./cipher/cipher";
+import { decrypt, encrypt, generate } from "./cipher/cipher";
 import { PrivateKeyJwk, Data } from "./types";
 import * as Types from "./types";
 
@@ -12,12 +12,26 @@ export const encryptQuery = (query: any, privateKeyJwk: PrivateKeyJwk) => {
   return hashedQuery;
 };
 
-export const encryptData = (data: Data, privateKeyJwk: PrivateKeyJwk) => {
-  const encryptedData = createEncryptedObject(data, privateKeyJwk);
-  return encryptedData;
+export const encryptData = (
+  data: Data | any,
+  privateKeyJwk: PrivateKeyJwk
+): any => {
+  const type = typeof data;
+  if (Array.isArray(data)) {
+    return data.map((d: any) => encryptData(d, privateKeyJwk));
+  }
+
+  if (type === "object") {
+    const encryptedData = createEncryptedObject(data, privateKeyJwk);
+    return encryptedData;
+  }
+  return encrypt(data, privateKeyJwk as PrivateKeyJwk);
 };
 
-export const decryptData = (data: any, privateKeyJwk: PrivateKeyJwk): any => {
+export const decryptData = (
+  data: Data | any,
+  privateKeyJwk: PrivateKeyJwk
+): any => {
   const type = typeof data;
   if (Array.isArray(data)) {
     return data.map((d: any) => decryptData(d, privateKeyJwk));
